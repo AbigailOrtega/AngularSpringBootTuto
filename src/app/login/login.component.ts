49abb4fda6fe5f4ae4550584c4601a49654ccbc2
Abigail Ortega Valdivia <Abigail.Ortega.Valdivia@mx.ey.com>
2019-05-18 00:25:16 -0500

    add validation of loggin |:wq

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { invalid } from '@angular/compiler/src/render3/view/util';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +10,26 @@ export class LoginComponent implements OnInit {
  /* propiedades a los que se pueden hacer refernecia desde los modelos */
   username='in28minutess'
   password= ''
+  errorMessage='Invalid Crdentials'
+  invalidLogin=false
   constructor() { }
 
   ngOnInit() {
   }
 
   handleLogin(){
-    console.log(this.username);
-    console.log(this.password);
+   // console.log(this.username);
+    //console.log(this.password);
+    if(this.username==='in28minutess' && this.password==='dummy'){
+      this.invalidLogin=false
+    }else{
+      this.invalidLogin=true
+    }
   }
 }
+
+/**EXISTEN 3 TIPOS DE ENLACES CON  LOS ATRIBUTOS
+ * 1.- Enlace de propiedad a valor en vista
+ * 2.- Eventos
+ * 3.- Usando ngModel
+*/
